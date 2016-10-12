5bee5b3fbe189e4a5dee55c716ab87277eaf145a
Daniel Gofman <d.gofman@web.de>
2016-10-12 12:51:01 +0200

    initial commit - added structure

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta charset=utf-8 />
+  	<title></title>
+  	<link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
